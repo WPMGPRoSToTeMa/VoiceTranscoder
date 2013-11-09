37cd73cc7d1f52e2d8aa6461dbeedaf0c8e790b9
Artem Golubikhin <WPMGPRoSToTeMa@users.noreply.github.com>
2013-11-10 02:53:53 +0400

    - Switched to dproto API and updated SILK .lib

--- VoiceCodecManager/voicecodecmanager.cpp
+++ VoiceCodecManager/voicecodecmanager.cpp
@@ -1,15 +1,7 @@
 #include "voicecodecmanager.h"
+#include "dproto_api.h"
 
-DYNLIB *g_pdlEngine;
-
-HOOKER< SV_PARSEVOICEDATA > *g_phookParseVoiceData;
-HOOKER< SV_WRITEVOICECODEC > *g_phookWriteVoiceCodec;
-
-MSG_READSHORT *g_pfnReadShort;
-MSG_READBUF *g_pfnReadBuf;
-MSG_WRITEBYTE *g_pfnWriteByte;
-MSG_WRITESHORT *g_pfnWriteShort;
-MSG_WRITEBUF *g_pfnWriteBuf;
+void (* g_pfnDefCallback)(client_t *);
 
 size_t g_sizeClientStruct;
 
@@ -29,23 +21,49 @@ cvar_t *g_pcvarVoiceQuality;
 cvar_t *g_pcvarVoiceVolumeSpeex;
 cvar_t *g_pcvarVoiceVolumeSilk;
 
-bool g_bIsMiles;
+size_t *pmsg_readcount;
+int *pmsg_badread;
+sizebuf_t *pnet_message;
+void * (* SZ_GetSpace)(sizebuf_t *, size_t);
+
+bool g_bIsntSpeex;
+
+dp_enginfo_api_t *g_pDpApi;
 
 qboolean VCM_Init( void ) {
-	g_pdlEngine = new DYNLIB( (void *)g_engfuncs.pfnPrecacheModel );
+	// Get dproto API
+	sscanf(CVAR_GET_STRING(DPROTO_API_CVAR_NAME), "%u", &g_pDpApi);
 
-	ParseSizeClientStruct();
+	// Check major ver
+	if (g_pDpApi->version_major != DPROTO_ENGINFO_API_VERSION_MAJOR) {
+		LOG_ERROR(PLID, "DPAPI major version checking failed: current %d (needed %d)", g_pDpApi->version_major, DPROTO_ENGINFO_API_VERSION_MAJOR);
 
-	g_phookParseVoiceData = new HOOKER<SV_PARSEVOICEDATA>(g_pdlEngine->FindAddr(SEARCH_SV_PARSEVOICEDATA), SV_ParseVoiceData);
-	g_phookWriteVoiceCodec = new HOOKER<SV_WRITEVOICECODEC>(g_pdlEngine->FindAddr(SEARCH_SV_WRITEVOICECODEC), SV_WriteVoiceCodec);
+		return false;
+	}
+	// Check minor ver
+	if (g_pDpApi->version_minor < DPROTO_ENGINFO_API_VERSION_MINOR) {
+		LOG_ERROR(PLID, "DPAPI minor version checking failed: current %d (needed minimal %d)", g_pDpApi->version_minor, DPROTO_ENGINFO_API_VERSION_MINOR);
 
-	g_pfnReadShort = (MSG_READSHORT *)g_pdlEngine->FindAddr(SEARCH_MSG_READSHORT);
-	g_pfnReadBuf = (MSG_READBUF *)g_pdlEngine->FindAddr(SEARCH_MSG_READBUF);
-	g_pfnWriteByte = (MSG_WRITEBYTE *)g_pdlEngine->FindAddr(SEARCH_MSG_WRITEBYTE);
-	g_pfnWriteShort = (MSG_WRITESHORT *)g_pdlEngine->FindAddr(SEARCH_MSG_WRITESHORT);
-	g_pfnWriteBuf = (MSG_WRITEBUF *)g_pdlEngine->FindAddr(SEARCH_MSG_WRITEBUF);
+		return false;
+	}
 
-	g_psvs = (server_static_t *)g_pdlEngine->FindAddr(SEARCH_SVS);
+	// Change clc_voicedata callback
+	sv_clcfuncs_t *pCLC = (sv_clcfuncs_t *)g_pDpApi->p_clc_funcs;
+
+	g_pfnDefCallback = pCLC[CLC_VOICEDATA].pfnCallback;
+	pCLC[CLC_VOICEDATA].pfnCallback = &SV_ParseVoiceData;
+
+	// Get size client_t
+	g_sizeClientStruct = g_pDpApi->client_t_size;
+
+	// Get svs
+	g_psvs = (server_static_t *)g_pDpApi->p_svs;
+
+	// ...
+	pmsg_readcount = (size_t *)g_pDpApi->p_msg_readcount;
+	pmsg_badread = g_pDpApi->p_msg_badread;
+	pnet_message = (sizebuf_t *)g_pDpApi->p_net_message_addr;
+	SZ_GetSpace = (void *(*)(sizebuf_t *, size_t))g_pDpApi->p_SZ_GetSpace;
 
 	CVAR_REGISTER(&g_cvarVoiceVolumeSpeex);
 	CVAR_REGISTER(&g_cvarVoiceVolumeSilk);
@@ -89,22 +107,22 @@ qboolean VCM_Init( void ) {
 		}
 	}
 
-	if (!strcmp(g_pcvarVoiceCodec->string, "voice_miles")) {
-		LOG_MESSAGE( PLID, "Warning vcodec is miles. Voice disabled." );
+	if (strcmp(g_pcvarVoiceCodec->string, "voice_speex") != 0) {
+		LOG_MESSAGE( PLID, "Warning vcodec isn't speex. Voice disabled." );
 
-		g_bIsMiles = true;
+		g_bIsntSpeex = true;
 	} else {
-		g_bIsMiles = false;
+		g_bIsntSpeex = false;
 	}
 
 	return TRUE;
 }
 
 qboolean VCM_End( void ) {
-	delete g_phookParseVoiceData;
-	delete g_phookWriteVoiceCodec;
+	// Change clc_voicedata callback
+	sv_clcfuncs_t *pCLC = (sv_clcfuncs_t *)g_pDpApi->p_clc_funcs;
 
-	delete g_pdlEngine;
+	pCLC[CLC_VOICEDATA].pfnCallback = g_pfnDefCallback;
 
 	for (int i = 0; i < MAX_CLIENTS; i++) {
 		g_pVoiceSpeex[i]->Release();
@@ -114,36 +132,62 @@ qboolean VCM_End( void ) {
 	return TRUE;
 }
 
-void ParseSizeClientStruct(void) {
-#ifdef _WIN32
-	g_sizeClientStruct = sizeof(client_t);
-#else
-	void *pAddr = g_pdlEngine->FindAddr(SEARCH_SV_CHECKTIMEOUTS);
+int MSG_ReadShort(void) {
+	short iResult;
 
-	pAddr = g_pdlEngine->FindAddr(pAddr, "\x81\x00\x00\x00\x00\x00", "\xFF\x00\x00\x00\xFF\xFF", 6);
+	if ( *pmsg_readcount + sizeof(short) > pnet_message->cursize )
+	{
+		*pmsg_badread = 1;
 
-	g_sizeClientStruct = *(size_t *)((size_t)pAddr + 2);
+		iResult = -1;
+	}
+	else
+	{
+		iResult = *(short *)&(pnet_message->data[*pmsg_readcount]);
 
-	LOG_MESSAGE( PLID, "Size of client_t structure is %d", g_sizeClientStruct );
-#endif
+		*pmsg_readcount += sizeof(short);
+	}
+
+	return iResult;
 }
 
-void SV_WriteVoiceCodec( sizebuf_t *pDatagram ) {
-	g_phookWriteVoiceCodec->Unhook( );
-	g_phookWriteVoiceCodec->m_pfnOrig( pDatagram );
-	g_phookWriteVoiceCodec->Hook( );
+int MSG_ReadBuf(size_t size, void *pDest) {
+	int iResult;
 
-	for (int i = 0; i < MAX_CLIENTS; i++) {
-		g_pVoiceSpeex[i]->Init(g_pcvarVoiceQuality->value);
+	if ( *pmsg_readcount + size > pnet_message->cursize )
+	{
+		*pmsg_badread = 1;
+
+		iResult = -1;
+	}
+	else
+	{
+		memcpy(pDest, &pnet_message->data[*pmsg_readcount], size);
+
+		*pmsg_readcount += size;
+
+		iResult = 1;
 	}
 
-	if (!strcmp(g_pcvarVoiceCodec->string, "voice_miles")) {
-		LOG_MESSAGE( PLID, "Warning vcodec is miles. Voice disabled." );
+	return iResult;
+}
 
-		g_bIsMiles = true;
-	} else {
-		g_bIsMiles = false;
-	}
+void MSG_WriteByte(sizebuf_t *pDatagram, byte bVal) {
+	byte *pbData = (byte *)SZ_GetSpace(pDatagram, sizeof(byte));
+
+	*pbData = bVal;
+}
+
+void MSG_WriteShort(sizebuf_t *pDatagram, short sVal) {
+	short *psData = (short *)SZ_GetSpace(pDatagram, sizeof(short));
+
+	*psData = sVal;
+}
+
+void MSG_WriteBuf(sizebuf_t *pDatagram, size_t size, void *pBuf) {
+	void *pData = SZ_GetSpace(pDatagram, size);
+
+	memcpy(pData, pBuf, size);
 }
 
 void SV_ParseVoiceData(client_t *pClient) {
@@ -156,18 +200,18 @@ void SV_ParseVoiceData(client_t *pClient) {
 
 	iClient = ((size_t)pClient - (size_t)g_psvs->m_pClients) / g_sizeClientStruct;
 
-	nDataLength = g_pfnReadShort( );
+	nDataLength = MSG_ReadShort( );
 
 	if ( nDataLength > sizeof( chReceived ) ) {
 		return;
 	}
 
-	g_pfnReadBuf( nDataLength, chReceived );
+	MSG_ReadBuf( nDataLength, chReceived );
 
 	if ( g_PlayerVCodec[ iClient + 1 ].m_voiceCodec == VOICECODEC_NONE) {
 		return;
 	}
-	if (g_bIsMiles) {
+	if (g_bIsntSpeex) {
 		return;
 	}
 	if (g_pcvarVoiceEnable->value == 0.0f) {
@@ -187,8 +231,6 @@ void SV_ParseVoiceData(client_t *pClient) {
 				sSample++;
 			}
 		}
-
-		//nDataLength = g_pVoiceSpeex[iClient]->Compress(chDecompressed, nDecompressedSamples, chReceived, sizeof(chReceived), false);
 
 		nCompressedLength = g_pVoiceSilk[iClient]->Compress(chDecompressed, nDecompressedSamples, chCompressed, sizeof(chCompressed), false);
 	} else {
@@ -248,10 +290,10 @@ void SV_ParseVoiceData(client_t *pClient) {
 		// Is there room to write this data in?
 		if( (6 + nSend + pDestClient->m_Datagram.cursize) < pDestClient->m_Datagram.maxsize )
 		{
-			g_pfnWriteByte( &pDestClient->m_Datagram, 53 );
-			g_pfnWriteByte( &pDestClient->m_Datagram, iClient );
-			g_pfnWriteShort( &pDestClient->m_Datagram, nSendLength );
-			g_pfnWriteBuf( &pDestClient->m_Datagram, nSendLength, chSend );
+			MSG_WriteByte( &pDestClient->m_Datagram, SVC_VOICEDATA );
+			MSG_WriteByte( &pDestClient->m_Datagram, iClient );
+			MSG_WriteShort( &pDestClient->m_Datagram, nSendLength );
+			MSG_WriteBuf( &pDestClient->m_Datagram, nSendLength, chSend );
 		}
 	}
 }

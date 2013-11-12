8286131a3f843aa2d3c51cc8409a6cb5de52d609
artem <artem@691d0cdc-21ef-4f2c-98ec-1882273ed600>
2013-11-12 17:46:08 +0000

    - Remove some debug code

--- trunk/DProtoAPI.cpp
+++ trunk/DProtoAPI.cpp
@@ -11,11 +11,11 @@ bool DProtoAPI_Init(void) {
 	// Get dproto API
 	const char *pszDpApi = CVAR_GET_STRING(DPROTO_API_CVAR_NAME);
 
-	LOG_MESSAGE(PLID, "%s = %s", DPROTO_API_CVAR_NAME, pszDpApi);
+	//LOG_MESSAGE(PLID, "%s = %s", DPROTO_API_CVAR_NAME, pszDpApi);
 
 	// Check for dpapi
 	if (pszDpApi[0] == '\0' || pszDpApi[0] == '0') {
-		LOG_ERROR(PLID, "Can't get DPAPI");
+		//LOG_ERROR(PLID, "Can't get DPAPI");
 
 		return false;
 	}
@@ -23,17 +23,17 @@ bool DProtoAPI_Init(void) {
 	// Scan for addr
 	sscanf(pszDpApi, "%u", &g_pDprotoAPI);
 
-	LOG_MESSAGE(PLID, "Found DprotoAPI at %.8X", g_pDprotoAPI);
+	//LOG_MESSAGE(PLID, "Found DprotoAPI at %.8X", g_pDprotoAPI);
 
 	// Check major ver
 	if (g_pDprotoAPI->version_major != DPROTO_ENGINFO_API_VERSION_MAJOR) {
-		LOG_ERROR(PLID, "DPAPI major version checking failed: current %d (needed %d)", g_pDprotoAPI->version_major, DPROTO_ENGINFO_API_VERSION_MAJOR);
+		//LOG_ERROR(PLID, "DPAPI major version checking failed: current %d (needed %d)", g_pDprotoAPI->version_major, DPROTO_ENGINFO_API_VERSION_MAJOR);
 
 		return false;
 	}
 	// Check minor ver
 	if (g_pDprotoAPI->version_minor < DPROTO_ENGINFO_API_VERSION_MINOR) {
-		LOG_ERROR(PLID, "DPAPI minor version checking failed: current %d (needed minimal %d)", g_pDprotoAPI->version_minor, DPROTO_ENGINFO_API_VERSION_MINOR);
+		//LOG_ERROR(PLID, "DPAPI minor version checking failed: current %d (needed minimal %d)", g_pDprotoAPI->version_minor, DPROTO_ENGINFO_API_VERSION_MINOR);
 
 		return false;
 	}

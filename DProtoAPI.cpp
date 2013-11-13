f6af386cfa7f475b598dabb25b25ea19b163458e
Artem Golubikhin <WPMGPRoSToTeMa@users.noreply.github.com>
2013-11-13 11:29:18 +0000

    - Add compile_debug.sh - Add debug messages for new compile mode - Fixed bug with sv_voicefloodms cvar

--- DProtoAPI.cpp
+++ DProtoAPI.cpp
@@ -11,11 +11,15 @@ bool DProtoAPI_Init(void) {
 	// Get dproto API
 	const char *pszDpApi = CVAR_GET_STRING(DPROTO_API_CVAR_NAME);
 
-	//LOG_MESSAGE(PLID, "%s = %s", DPROTO_API_CVAR_NAME, pszDpApi);
+#ifdef VTC_DEBUG
+	LOG_MESSAGE(PLID, "%s = %s", DPROTO_API_CVAR_NAME, pszDpApi);
+#endif
 
 	// Check for dpapi
 	if (pszDpApi[0] == '\0' || pszDpApi[0] == '0') {
-		//LOG_ERROR(PLID, "Can't get DPAPI");
+#ifdef VTC_DEBUG
+		LOG_ERROR(PLID, "Can't get DPAPI");
+#endif
 
 		return false;
 	}
@@ -23,17 +27,23 @@ bool DProtoAPI_Init(void) {
 	// Scan for addr
 	sscanf(pszDpApi, "%u", &g_pDprotoAPI);
 
-	//LOG_MESSAGE(PLID, "Found DprotoAPI at %.8X", g_pDprotoAPI);
+#ifdef VTC_DEBUG
+	LOG_MESSAGE(PLID, "Found DprotoAPI at %.8X", g_pDprotoAPI);
+#endif
 
 	// Check major ver
 	if (g_pDprotoAPI->version_major != DPROTO_ENGINFO_API_VERSION_MAJOR) {
-		//LOG_ERROR(PLID, "DPAPI major version checking failed: current %d (needed %d)", g_pDprotoAPI->version_major, DPROTO_ENGINFO_API_VERSION_MAJOR);
+#ifdef VTC_DEBUG
+		LOG_ERROR(PLID, "DPAPI major version checking failed: current %d (need %d)", g_pDprotoAPI->version_major, DPROTO_ENGINFO_API_VERSION_MAJOR);
+#endif
 
 		return false;
 	}
 	// Check minor ver
 	if (g_pDprotoAPI->version_minor < DPROTO_ENGINFO_API_VERSION_MINOR) {
-		//LOG_ERROR(PLID, "DPAPI minor version checking failed: current %d (needed minimal %d)", g_pDprotoAPI->version_minor, DPROTO_ENGINFO_API_VERSION_MINOR);
+#ifdef VTC_DEBUG
+		LOG_ERROR(PLID, "DPAPI minor version checking failed: current %d (need minimal %d)", g_pDprotoAPI->version_minor, DPROTO_ENGINFO_API_VERSION_MINOR);
+#endif
 
 		return false;
 	}

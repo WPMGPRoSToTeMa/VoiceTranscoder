c98b023f4c34b6989e8c20ccc67ff7bab576e2e7
Artem <Artem@691d0cdc-21ef-4f2c-98ec-1882273ed600>
2017-02-24 18:01:21 +0000

    I hope this should fix build

--- trunk/Main.cpp
+++ trunk/Main.cpp
@@ -338,7 +338,9 @@ void OnFrameStarted() {
 			int16_t *samplesSlice8k = &playSound.samples8k[playSound.currentSample];
 			size_t sampleCount = min(playSound.samples8k.size() - playSound.currentSample, (size_t)160);
 			playSound.currentSample += sampleCount;
-			playSound.OldEncodedDataSendTime += 20ms - 125us; // TODO: ..., 1/8000 for 8k NS good! for 16k S bad, for 8k S very bad!!!
+			//playSound.OldEncodedDataSendTime += 20ms - 125us; // TODO: ..., 1/8000 for 8k NS good! for 16k S bad, for 8k S very bad!!!
+			playSound.OldEncodedDataSendTime += 20ms;
+			playSound.OldEncodedDataSendTime -= 125us;
 
 			if (sampleCount != 0) {
 				array<uint8_t, 1024> oldEncodedData;
@@ -377,7 +379,9 @@ void OnFrameStarted() {
 			int16_t *samplesSlice16k = &playSound.samples16k[playSound.PlayedNewEncodedSampleCount];
 			size_t sampleCount = min(playSound.samples16k.size() - playSound.PlayedNewEncodedSampleCount, (size_t)320);
 			playSound.PlayedNewEncodedSampleCount += sampleCount;
-			playSound.NewEncodedDataSendTime += 20ms - 137us; // TODO: ..., 1/8000 for 8k NS good! for 16k S bad, for 8k S very bad!!!
+			//playSound.NewEncodedDataSendTime += 20ms - 137us; // TODO: ..., 1/8000 for 8k NS good! for 16k S bad, for 8k S very bad!!!
+			playSound.OldEncodedDataSendTime += 20ms;
+			playSound.OldEncodedDataSendTime -= 137us;
 			// chut chut rastet
 			// vector vilet v konce!!
 

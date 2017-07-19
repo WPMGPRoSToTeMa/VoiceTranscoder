fb86e68462433b44e5da52414b440a3cbe3ed636
Artem <Artem@691d0cdc-21ef-4f2c-98ec-1882273ed600>
2017-07-19 10:07:14 +0000

    Try this

--- trunk/VoiceCodecs/Opus/config.h
+++ trunk/VoiceCodecs/Opus/config.h
@@ -37,9 +37,9 @@ POSSIBILITY OF SUCH DAMAGE.
 
 //#if defined(_M_IX86) || defined(_M_X64)
 /* Can always compile SSE intrinsics (no special compiler flags necessary) */
-#define OPUS_X86_MAY_HAVE_SSE
-#define OPUS_X86_MAY_HAVE_SSE2
-#define OPUS_X86_MAY_HAVE_SSE4_1
+//#define OPUS_X86_MAY_HAVE_SSE
+//#define OPUS_X86_MAY_HAVE_SSE2
+//#define OPUS_X86_MAY_HAVE_SSE4_1
 
 /* Presume SSE functions, if compiled to use SSE/SSE2/AVX (note that AMD64 implies SSE2, and AVX
    implies SSE4.1) */
@@ -47,7 +47,7 @@ POSSIBILITY OF SUCH DAMAGE.
 //#define OPUS_X86_PRESUME_SSE 1
 //#endif
 //#if defined(_M_X64) || (defined(_M_IX86_FP) && (_M_IX86_FP >= 2)) || defined(__AVX__)
-#define OPUS_X86_PRESUME_SSE2 1
+//#define OPUS_X86_PRESUME_SSE2 1
 //#endif
 //#if defined(__AVX__)
 //#define OPUS_X86_PRESUME_SSE4_1 1
